dfd8f4942e9f65848e57f51145adf262d93afe3b
devinbresser <66394890+devinbresser@users.noreply.github.com>
2024-04-14 16:58:04 -0500

    Update train-binary-addition.py

--- config/train-binary-addition.py
+++ config/train-binary-addition.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-binary-addition-char'
+out_dir = 'out-binary-additions-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
